b044cb7f9793afc1c45573facd95b5bc79e0d62a
Weston Nyirenda <weston.nyirenda@integrove.com>
2022-02-11 11:43:09 +0200

    adding content on homepage

--- Frontend/pages/home.js
+++ Frontend/pages/home.js
@@ -1,1 +1,9 @@
-const home = { template: `<h1>This is Home</h1>` };
+const home = {
+    template: ` 
+    <div class="px-4 py-5 my-5 text-center">
+        <h1 class="display-5 fw-bold">Welcome to the Church Management System</h1>
+        <p class="lead ">This is a simple system that allows churches to keep and maintain a database of its church member.<br> Here you will be able to add, edit and delete a record of a church member as well as allocate them to a department</p>
+
+    </div>
+    `,
+};
