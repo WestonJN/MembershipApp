283fc0ff9106cfa3b8cb440ec660998a740cb8ae
Weston Nyirenda <weston.nyirenda@integrove.com>
2022-02-14 11:12:46 +0200

    Added complinace controller and models so that we can query the database for covid compliance of each member. Wrong branch i admit.

---
+++ MembershipApp/Models/Compliance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MembershipApp.Models
+{
+    public class Compliance
+    {
+        public int ComplianceId { get; set; }
+
+        public string MemberFullName { get; set; }
+
+        public string Temperature { get; set; }
+
+        public string Date { get; set; }
+
+        public string Fever { get; set; }
+
+        public string Chills { get; set; }
+
+        public string Breath { get; set; }
+
+        public string Cough { get; set; }
+
+        public string Taste { get; set; }
+
+        public string Contact { get; set; }
+
+        public string Other { get; set; }
+
+    }
+}
